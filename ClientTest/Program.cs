c81c6176d520e4fa20da708e28c32f8aec2fc589
matakom <mata.komarek@gmail.com>
2023-02-13 19:05:14 +0100

    server and client test
    Uploaded server and client test. Changed 'rámcová_analýza.md' to 'README.md' a deleted previous readme

---
+++ ClientTest/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+using System.Threading;
+
+namespace ClientTest
+{
+    internal class Program
+    {
+
+        static void Main()
+        {
+            ConnectToServer();
+        }
+        static async void ConnectToServer()
+        {
+            Uri serverUri = new Uri("ws://localhost:5000");
+            //Implementation of timeout of 5000 ms
+            var source = new CancellationTokenSource();
+            source.CancelAfter(500000);
+
+            using (ClientWebSocket myWebSocket = new ClientWebSocket())
+            {
+
+                try
+                {
+                    myWebSocket.ConnectAsync(serverUri, source.Token).Wait(source.Token);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception:" + e);
+                    Console.ReadKey();
+                }
+                Console.WriteLine("Connected to server\n");
+                while (myWebSocket.State == WebSocketState.Open)
+                {
+                    string message = Console.ReadLine();
+                    byte[] messageyBytes = Encoding.UTF8.GetBytes(message);
+                    await myWebSocket.SendAsync(new ArraySegment<byte>(messageyBytes), WebSocketMessageType.Text, true, source.Token);
+                    ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+                    WebSocketReceiveResult result = await myWebSocket.ReceiveAsync(buffer, CancellationToken.None);
+                    message = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+                    Console.WriteLine(message);
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ serverTest/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net.WebSockets;
+using System.Threading;
+using System.Security.Policy;
+using System.Diagnostics;
+
+namespace serverTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string port = "5000";
+            string url = "http://localhost:" + port + "/";
+            HttpListener listener = new HttpListener();
+            listener.Prefixes.Add(url);
+            listener.Start();
+            Console.WriteLine("Waiting for WS connection...\n");
+            Console.WriteLine(url + "\n");
+
+            while (true)
+            {
+                HttpListenerContext context = listener.GetContext();
+                if (context.Request.IsWebSocketRequest)
+                {
+                    ProcessRequest(context);
+                }
+            }
+        }
+        static async void ProcessRequest(HttpListenerContext context)
+        {
+            var source = new CancellationTokenSource();
+            source.CancelAfter(500000);
+            var webSocketContext = await context.AcceptWebSocketAsync(subProtocol: null);
+            WebSocket serverWebSocket = webSocketContext.WebSocket;
+            Console.WriteLine("WS connected");
+            while (serverWebSocket.State == WebSocketState.Open)
+            {
+                ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+                Console.WriteLine(serverWebSocket.State);
+                WebSocketReceiveResult result = await serverWebSocket.ReceiveAsync(buffer, source.Token);
+                string message = Encoding.UTF8.GetString(buffer.Array, 0, result.Count);
+                Console.WriteLine(message);
+
+                switch (message)
+                {
+                    case "ahoj":
+                        message = "Zdravím tě dobrodruhu";
+                        break;
+                    case "matyáš je frajer":
+                        message = "Hahaha. ten byl dobrej";
+                        break;
+                }
+
+                buffer = new ArraySegment<byte>(Encoding.UTF8.GetBytes(message));
+                Console.WriteLine(serverWebSocket.State);
+                try
+                {
+                    await serverWebSocket.SendAsync(buffer, WebSocketMessageType.Text, true, source.Token);
+                    Console.WriteLine("Sent: " + message);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine(e);
+                    Console.ReadKey();
+                }
+            }
+        }
+
+
+    }
+}
